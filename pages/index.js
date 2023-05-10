ebdf9a0a171e2c2209418f07b1db00463f79c040
lucassantos22 <lucas.afsantos@hotmail.com>
2023-05-10 00:05:35 -0300

    chore: add logo and home page design

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
+import logo from '../src/assets/me.jpeg'
+
 export default function() {
   return (
     <div class="has-background-white-ter">
-      <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-        <div style={{ padding: '0rem 3rem' }}>
-          <p class="is-size-1 has-text-centered">Hi, my name is <strong>Lucas</strong>!</p>
-          <p class="is-size-3 has-text-centered mt-2">I'm a software engineer focused on Front-end technologies</p>
-          <p class="is-size-5 has-text-centered is-italic mt-5">HTML5, CSS3, Javascript, Typescript, React.js, Vue.js, Next.js, Node.js, Express, Pinia, Firebase, MongoDB, Jest, Cypress, Tailwind.css, BULMA, Bootstrap, Github, Jenkins, Git, SCRUM, Kanban... and a litle bit more :)</p>
-          <button class="button mt-6" style={{ margin: '0 auto', display: 'block', background: 'linear-gradient(157deg, rgba(2,0,36,1) 0%, rgba(46,42,42,1) 0%, rgba(0,0,0,1) 100%)', color: 'white' }}>Contact me! - it's free ;)</button>
+      <div class="has-text-black" style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+        <div style={{ padding: '0rem 4rem' }}>
+          <img src={logo} width={150} height={150} style={{ borderRadius: '100%', margin: '0 auto', display: 'block' }}></img>
+          <p class="is-size-2 has-text-centered mt-4">Hello there, my name is <strong>Lucas</strong>!</p>
+          <p class="is-size-3 has-text-centered">I'm a software engineer focused on Front-end technologies</p>
+          <p class="is-size-6 has-text-centered is-italic mt-4">HTML5, CSS3, Javascript, Typescript, React.js, Vue.js, Next.js, Node.js, Express, Pinia, Firebase, MongoDB, Jest, Cypress, Tailwind.css, BULMA, Bootstrap, Github, Jenkins, Git, SCRUM, Kanban... and a litle bit more :)</p>
+          <button class="button mt-6 has-background-black-ter is-rounded has-text-light" style={{ margin: '0 auto', display: 'block' }}>Contact me! - it's free ;)</button>
         </div>
       </div>
-      <div class="has-text-white-bis has-background-black-ter" style={{ height: '100vh' }}>
-        <p class="is-size-2 has-text-centered mt-6">Developing since 2018</p>
+      <div class="has-text-white-bis has-background-black-ter" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100vh' }}>
+        <p class="is-size-2 has-text-centered">Developing since 2018</p>
       </div>
     </div>
 
