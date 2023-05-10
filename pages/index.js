9f87b666da458ce6bc3ff6188b1ad9c64a08abd7
lucassantos22 <lucas.afsantos@hotmail.com>
2023-05-09 21:23:40 -0300

    chore: change colors

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 export default function() {
   return (
-    <div>
+    <div class="has-background-white-ter">
       <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
         <div style={{ padding: '0rem 3rem' }}>
           <p class="is-size-1 has-text-centered">Hi, my name is <strong>Lucas</strong>!</p>
           <p class="is-size-3 has-text-centered mt-2">I'm a software engineer focused on Front-end technologies</p>
           <p class="is-size-5 has-text-centered is-italic mt-5">HTML5, CSS3, Javascript, Typescript, React.js, Vue.js, Next.js, Node.js, Express, Pinia, Firebase, MongoDB, Jest, Cypress, Tailwind.css, BULMA, Bootstrap, Github, Jenkins, Git, SCRUM, Kanban... and a litle bit more :)</p>
-          <button class="button mt-6 is-dark" style={{ margin: '0 auto', display: 'block' }}>Contact me! - it's free ;)</button>
+          <button class="button mt-6" style={{ margin: '0 auto', display: 'block', background: 'linear-gradient(157deg, rgba(2,0,36,1) 0%, rgba(46,42,42,1) 0%, rgba(0,0,0,1) 100%)', color: 'white' }}>Contact me! - it's free ;)</button>
         </div>
       </div>
-      <div class="has-text-white-bis has-background-black-ter" style={{ height: '99vh' }}>
+      <div class="has-text-white-bis has-background-black-ter" style={{ height: '100vh' }}>
         <p class="is-size-2 has-text-centered mt-6">Developing since 2018</p>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,18 @@
+import Image from 'next/image';
+import logo from '../assets/logo.svg'
+
 export const Navbar = () => {
   return (
-    <nav class="navbar has-background-black-ter" role="navigation" aria-label="main navigation" style={{ position: 'fixed', width: '100vw', padding: '0 6rem', border: '.5px solid black' }}>
+    <nav class="navbar has-background-black-ter" role="navigation" aria-label="main navigation" style={{ position: 'fixed', width: '100vw', padding: '0 6rem', border: '.5px solid black', background: 'linear-gradient(157deg, rgba(2,0,36,1) 0%, rgba(46,42,42,1) 0%, rgba(0,0,0,1) 100%)' }}>
     <div class="navbar-brand">
-        <p class="is-size-5 mt-4 mb-4 has-text-white-bis is-family-monospace has-text-weight-bold">LUCAS SANTOS</p>
+        <Image alt="Lucas Santos logo" width='100px' height='40px' src={logo}></Image>
     </div>
 
     <div id="navbarBasicExample" class="navbar-menu">
         <div class="navbar-end">
         <div class="navbar-item">
             <div class="buttons">
-            <button class="button is-dark is-inverted">Contact</button>
+            <button class="button has-background-white-ter is-inverted">Contact</button>
             </div>
         </div>
         </div>
