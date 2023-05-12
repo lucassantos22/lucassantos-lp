e9ba11ff24d57212c63a817cd7122d33ada3ca78
lucassantos22 <lucas.afsantos@hotmail.com>
2023-05-11 23:38:55 -0300

    chore: add responsitivy

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function() {
     <div class="has-background-white-ter">
       <div class="has-text-black container">
         <div class="content">
-          <img class="my-picture" src={logo} width={150} height={150} />
+          <img class="my-picture" src={logo} />
           <p class="is-size-2 has-text-centered mt-4">Hello there, my name is <strong>Lucas</strong>!</p>
           <p class="is-size-3 has-text-centered">I'm a software engineer focused on Front-end technologies</p>
           <p class="is-size-6 has-text-centered is-italic mt-4">HTML5, CSS3, Javascript, Typescript, React.js, Vue.js, Next.js, Node.js, Express, Pinia, Firebase, MongoDB, Jest, Cypress, Tailwind.css, BULMA, Bootstrap, Github, Jenkins, Git, SCRUM, Kanban... and a litle bit more :)</p>
