c68baa34244a3b01a5430325bc5979fed8700284
lucassantos22 <lucas.afsantos@hotmail.com>
2023-05-14 19:19:17 -0300

    chore: refactor segware section

--- src/components/sections/segware.js
+++ src/components/sections/segware.js
@@ -1,16 +1,14 @@
 import segwareLogoDark from '../../../src/assets/segware-logo-dark.svg'
 
 export const SegwareSection = () => (
-    <div class="has-background-white-ter" id="segware">
-        <div class="container has-text-black">
-        <div class="content">
+    <div class="has-background-white-ter has-text-black" id="segware">
+        <div class="container ">
             <img class="center" src={segwareLogoDark} width={300} height={300} />
             <p class="is-size-3 has-text-centered">2017</p>
             <p class="mt-6">I started my career as an intern in technical support,
             performing tasks such as direct client remote access, MySQL database backups, and so on.
             The experience brought me a direct relationship with the client and helped a lot in my personal development.
             </p>
-        </div>
         </div>
   </div>
 )
