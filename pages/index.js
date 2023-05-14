41732d5e039d7295529b6f7b3b99a9a198e63d37
lucassantos22 <lucas.afsantos@hotmail.com>
2023-05-14 01:06:55 -0300

    chore

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import logo from '../src/assets/me.jpeg'
 import segwareLogo from '../src/assets/segware-logo.svg'
+import segwareLogoDark from '../src/assets/segware-logo-dark.svg'
 import digitroLogo from '../src/assets/digitro-logo.svg'
 import zygoLogo from '../src/assets/zygo-logo.svg'
 import pagseguroLogo from '../src/assets/pagseguro-logo.svg'
@@ -22,7 +23,10 @@ export default function() {
         <p class="is-size-2 has-text-centered">My Professional Experience</p>
         <div class="grid-timeline">
           <div>
-            <img class="mt-5" src={segwareLogo} width={150} height={150} /> 2017 - 2017
+            <a href="#segware">
+              <img class="mt-5" src={segwareLogo} width={150} height={150} />
+            </a>
+            2017 - 2017
           </div>
           <div>
             <img class="mt-5" src={digitroLogo} width={150} height={150} /> 2018 - 2020
@@ -36,7 +40,22 @@ export default function() {
         </div>
       </div>
 
-      <div class="has-text-black container">
+      <div class="has-background-white-ter container" id="segware">
+        <div class="content">
+          <img class="center" src={segwareLogoDark} width={300} height={300} />
+          <p class="is-size-3 has-text-centered">2017</p>
+          <p class="mt-6">I started my career as an intern in technical support,
+            performing tasks such as direct client remote access, MySQL database backups, and so on.
+            The experience brought me a direct relationship with the client and helped a lot in my personal development.
+          </p>
+        </div>
+      </div>
+
+      <div class="second-container has-text-white-bis has-background-black-ter">
+        <img class="center" src={digitroLogo} width={300} height={300} />
+      </div>
+
+      <div class="has-background-white-ter container">
         <p class="is-size-2 mb-6">Scan to know me better!</p>
         <img style={{ height: '200px', width: '200px', display: 'block' }} src={qrcode} />
       </div>
