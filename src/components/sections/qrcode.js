e22b5df6b0bb0b427780c2ff8bf682480cc8551a
lucassantos22 <lucas.afsantos@hotmail.com>
2023-05-14 01:45:06 -0300

    chore: fix qr code

--- src/components/sections/qrcode.js
+++ src/components/sections/qrcode.js
@@ -1,8 +1,10 @@
 import qrcode from '../../../src/assets/qrcode.svg'
 
 export const QrCodeSection = () => (
-    <div class="has-background-white-ter container">
-        <p class="is-size-2 mb-6">Scan to know me better!</p>
-        <img style={{ height: '200px', width: '200px', display: 'block' }} src={qrcode} />
+    <div class="has-background-white-ter">
+        <div class="container">
+            <p class="is-size-2 mb-6">Scan to know me better!</p>
+            <img style={{ height: '200px', width: '200px', display: 'block' }} src={qrcode} />
+        </div>
     </div>
 )
