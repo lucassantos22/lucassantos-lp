f55ec7c160954f7858c786e7bfd7f5879c1fd40c
lucassantos22 <lucas.afsantos@hotmail.com>
2023-05-14 19:20:21 -0300

    chore: finish digitro section

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,17 @@ export default function() {
       <MainSection />
       <ProfessionalTimelineSection />
       <SegwareSection />
-        <div class="second-container has-text-white-bis has-background-black-ter" id="digitro">
-          <img class="center" src={digitroLogo} width={300} height={300} />
+      <div class="container has-text-white-bis has-background-black-ter" id="digitro">
+          <div class="content">
+            <img class="center" src={digitroLogo} width={300} height={300} />
+            <p class="is-size-3 has-text-centered">2018 - 2020</p>
+            <p class="mt-6">
+              At Digitro, I started as an intern in the development area, an activity that I identified myself with.
+              Development of web systems, bug fixes, automated tests, cross tests, database manipulation and contact
+              with agile methodologies are some of the experiences I have had.
+            </p>
         </div>
+      </div>
       <QrCodeSection />
 
     </>
