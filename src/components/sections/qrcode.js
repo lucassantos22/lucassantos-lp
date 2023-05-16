3240695f78129e51c75ad328e5448528d7ab2478
lucassantos22 <lucas.afsantos@hotmail.com>
2023-05-16 01:02:45 -0300

    chore: add id in qrcode

--- src/components/sections/qrcode.js
+++ src/components/sections/qrcode.js
@@ -2,7 +2,7 @@ import qrcode from '../../../src/assets/qrcode.svg'
 import { Section } from '../Section'
 
 export const QrCodeSection = () => (
-    <Section>
+    <Section id="qr-code">
         <div class="container">
             <p class="is-size-2 mb-6">Scan to know me better!</p>
             <img style={{ height: '200px', width: '200px', display: 'block' }} src={qrcode} />
