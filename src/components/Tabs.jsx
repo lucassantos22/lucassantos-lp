81e0b01489a50671bd759fbf38a3a432537871da
lucassantos22 <lucas.afsantos@hotmail.com>
2023-05-16 01:05:02 -0300

    chore: add Tabs.jsx

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Tabs = () => {
+    return (
+        <div class="tabs is-boxed is-centered">
+            <ul>
+                <li class="is-active"><a href="#me">Me</a></li>
+                <li><a href="#pro-experience">Pro Experience</a></li>
+                <li><a>Pro Projects</a></li>
+                <li><a>Personal Projects</a></li>
+                <li><a href="#qr-code">QR Code</a></li>
+            </ul>
+        </div>
+    )
+}
