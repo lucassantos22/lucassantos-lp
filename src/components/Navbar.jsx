cf34fb20c58ae58595c85e063951c1544f700a04
lucassantos22 <lucas.afsantos@hotmail.com>
2023-07-31 21:38:15 -0300

    fix: fix navbar image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
   return (
     <nav class="navbar has-background-white-ter" role="navigation" aria-label="main navigation" style={{ position: 'fixed', width: '100vw', padding: '0 6rem' }}>
     <div class="navbar-brand">
-        <Image alt="Lucas Santos logo" width='100px' height='40px' src={logo}></Image>
+        <Image alt="Lucas Santos logo" width={100} height={40} src={logo}></Image>
     </div>
 
     <div id="navbarBasicExample" class="navbar-menu">
